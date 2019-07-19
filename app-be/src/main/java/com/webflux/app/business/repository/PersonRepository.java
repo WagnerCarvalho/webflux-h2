79f1125e91664d832a81d54fbd4d78ba52d82b09
Wagner Carvalho <wagner.carvalho@mercadolivre.com>
2019-07-19 20:41:53 -0300

    GET and POST

--- app-be/src/main/java/com/webflux/app/business/repository/PersonRepository.java
+++ app-be/src/main/java/com/webflux/app/business/repository/PersonRepository.java
@@ -2,11 +2,8 @@ package com.webflux.app.business.repository;
 
 import com.webflux.app.business.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-  Optional<Person> findByEmail(String email);
+  Person findByEmail(String email);
+  Person findFirstByEmail(String email);
 }
--- app-be/src/main/java/com/webflux/app/business/service/PersonService.java
+++ app-be/src/main/java/com/webflux/app/business/service/PersonService.java
@@ -4,12 +4,8 @@ import com.webflux.app.business.domain.Person;
 import com.webflux.app.business.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
-
-import static org.springframework.web.reactive.function.BodyInserters.fromObject;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 import static reactor.core.publisher.Mono.fromCallable;
 
 @Service
@@ -18,17 +14,10 @@ public class PersonService {
   @Autowired
   private PersonRepository personRepository;
 
-  private Person persistPerson(final Person person) {
-    personRepository.save(person);
-    personRepository.findByEmail(person.getEmail());
-    return person;
-  }
-
   public Mono<Person> save(final Person person) {
-    return fromCallable(() -> personRepository.findByEmail(person.getEmail()).orElse(null))
+    return fromCallable(() -> personRepository.findFirstByEmail(person.getEmail()))
+            .defaultIfEmpty(personRepository.save(person))
             .publishOn(Schedulers.elastic())
-            .flatMap(Mono::justOrEmpty)
-            .map(currentPerson -> persistPerson(person))
-            .flatMap(this::save);
+            .flatMap(Mono::justOrEmpty);
   }
 }
